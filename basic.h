989d830e63616013dac04e2048c4391931ef3578
JTJL <ch.papaw@gmail.com>
2019-02-09 20:37:42 +0800

    update gitignore

--- basic.h
+++ basic.h
@@ -13,7 +13,7 @@ const int mod = 1e9 + 7;
 const int MM = 1e9 + 7;
 
 ll pw(ll p, ll q) {
-	p %= MM;
+    p %= MM;
     ll ret = 1;
     for (; q; q >>= 1) {
         if (q & 1) ret = ret * p % MM;
@@ -32,7 +32,7 @@ double pw(double p, ll q) {
 }
 
 ll pw(ll p, ll q, ll mod) {
-	p %= mod;
+    p %= mod;
     ll ret = 1;
     for (; q; q >>= 1) {
         if (q & 1) ret = ret * p % mod;
@@ -43,33 +43,33 @@ ll pw(ll p, ll q, ll mod) {
 
 template <typename T>
 void maxi(T& x, T y) {
-	x = max(x, y);
+    x = max(x, y);
 }
 
 template <typename T>
 void mini(T& x, T y) {
-	x = min(x, y);
+    x = min(x, y);
 }
 
 template <typename T>
 T gcd(T p, T q) {
-	return q ? gcd(q, p % q) : p;
+    return q ? gcd(q, p % q) : p;
 }
 
 ll euler_phi(ll n) {
-	ll ret = n;
-	for (ll i = 2; i * i <= n; ++i) 
-		if (n % i == 0) {
-			ret = ret / i * (i - 1);
-			while (n % i == 0) 
-				n /= i;
-		}
-	if (n > 1) 
-		ret = ret / n * (n - 1);
-	return ret;
+    ll ret = n;
+    for (ll i = 2; i * i <= n; ++i) 
+        if (n % i == 0) {
+            ret = ret / i * (i - 1);
+            while (n % i == 0) 
+                n /= i;
+        }
+    if (n > 1) 
+        ret = ret / n * (n - 1);
+    return ret;
 }
 
 template <typename T>
 T sqr(T x) {
-	return x * x;
+    return x * x;
 }
